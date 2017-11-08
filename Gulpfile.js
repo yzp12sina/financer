f7fba4b8ea9fc8884116971de143be54e6dfb1c5
Willians Echart <willians.echart@gmail.com>
2017-11-08 17:58:46 -0200

    first-commit

---
+++ Gulpfile.js
@@ -0,0 +1,54 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var cssnano = require('gulp-cssnano');
+var gulpIf = require('gulp-if');
+var imagemin = require('gulp-imagemin');
+
+gulp.task('default',function(){
+	return gulp.watch();
+})
+
+gulp.task('sass', function(){
+  return gulp.src('app/assets/scss/**/*.scss') // Gets all files ending with .scss in app/scss
+    .pipe(sass({includePaths: ['app/assets/scss']}).on('err', sass.logError))
+    .pipe(gulp.dest('app/assets/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+gulp.task('scripts',function(){
+	return gulp.src('app/assets/scss/**/*.js')
+		.pipe(gulpIf('**/*.js', uglify()))
+		.pipe(gulp.dest('app/assets/js/*.js'))
+		.pipe(browserSync.reload({
+	      stream: true
+	    }))
+});	
+gulp.task('images', function(){
+  return gulp.src('app/assets/images/**/*.+(png|jpg|gif|svg)')
+  .pipe(imagemin())
+  .pipe(gulp.dest('dist/images'))
+});
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+  })
+})
+gulp.task('build', ['sass','scripts','images'], function(){
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('**/*.js', uglify()))
+    .pipe(gulpIf('**/*.css', cssnano()))
+    .pipe(gulp.dest('dist'))
+});
+
+gulp.task('watch', ['browserSync', 'sass','scripts'], function (){
+  gulp.watch('app/assets/scss/**/*.scss', ['sass']);
+  gulp.watch('app/*.html', browserSync.reload); 
+  gulp.watch('app/assets/js/**/*.js', browserSync.reload);  
+});

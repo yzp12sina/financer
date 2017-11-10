a4ddcf5ed098800e449e7ce14313638021a862be
echart <willians.fagundes@hotmail.com>
2017-11-10 17:36:39 -0200

    menu rotative index.html

--- app/assets/js/index.js
+++ app/assets/js/index.js
@@ -1,1 +1,6 @@
-var z = 1;
+$('header nav a').click(function(){
+  var _this = this;
+  var url = $(_this).attr('href').replace('#','.content--');
+  if($(url).length)
+    $('html,body').animate({scrollTop: $(url).offset().top}, 2000);
+})

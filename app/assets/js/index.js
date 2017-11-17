28343e65321123c16f8d6682be33434e1c320bc6
echart <willians.fagundes@hotmail.com>
2017-11-17 11:13:20 -0200

    menu sandwich animation, still pending the mobile menu

--- app/assets/js/index.js
+++ app/assets/js/index.js
@@ -1,11 +1,17 @@
 //index navbar
 $('header nav a').click(function(){
-  var _this = this;
-  var url = $(_this).attr('href').replace('#','.content--');
+  var url = $(this).attr('href').replace('#','.content--');
   if($(url).length)
     $('html,body').animate({scrollTop: $(url).offset().top}, 2000);
 });
-//scroll menu color
-$(window).scroll(function() {
-    if ($(window).scrollTop() >= 200) $("header").addClass("scroll"); else $("header").removeClass("scroll");
+//scroll menu change header background-color
+$(window).scroll(function(){
+    if($(window).scrollTop()>=300) $("header").addClass("scroll"); else $("header").removeClass("scroll");
 });
+//menu mobile
+$('.menu_mobile--sandwich').on('click', function(){
+  $(this).toggleClass("menu_mobile--sandwich--open");
+  $('header nav').toggleClass('visible');
+  // $('nav').toggleClass('navbar-open');
+  // $('header').toggleClass('fixed');
+})
